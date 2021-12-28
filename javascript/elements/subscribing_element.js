e5727d36b140678920f93038297906f8b6c40191
Marco Roth <marco.roth@intergga.ch>
2021-12-29 00:12:58 +0100

    cable_ready/2e10aff update .gitignore

---
+++ javascript/elements/subscribing_element.js
@@ -0,0 +1,24 @@
+export default class SubscribingElement extends HTMLElement {
+  disconnectedCallback () {
+    if (this.channel) this.channel.unsubscribe()
+  }
+
+  createSubscription (consumer, channel, receivedCallback) {
+    this.channel = consumer.subscriptions.create(
+      {
+        channel,
+        identifier: this.getAttribute('identifier')
+      },
+      {
+        received: receivedCallback
+      }
+    )
+  }
+
+  get preview () {
+    return (
+      document.documentElement.hasAttribute('data-turbolinks-preview') ||
+      document.documentElement.hasAttribute('data-turbo-preview')
+    )
+  }
+}
