a4875e12b75268ee5f04da43dbbfc4c8d2bb20f3
Marco Roth <marco.roth@intergga.ch>
2021-12-30 20:19:50 +0000

    cable_ready/f3476df setup to use secret to push up tag

---
+++ javascript/index.js
@@ -0,0 +1,61 @@
+import { version } from '../package.json'
+import * as MorphCallbacks from './morph_callbacks'
+import { shouldMorphCallbacks, didMorphCallbacks } from './morph_callbacks'
+import * as Utils from './utils'
+import OperationStore, { addOperation, addOperations } from './operation_store'
+import { perform, performAsync } from './cable_ready'
+import StreamFromElement from './elements/stream_from_element'
+import UpdatesForElement from './elements/updates_for_element'
+import SubscribingElement from './elements/subscribing_element'
+import CableConsumer from './cable_consumer'
+
+const initialize = (initializeOptions = {}) => {
+  const { consumer } = initializeOptions
+
+  if (consumer) {
+    CableConsumer.setConsumer(consumer)
+  } else {
+    console.error(
+      'The `CableReady.initialize({ consumer })` call expects an ActionCable `consumer` to be passed in to function.'
+    )
+  }
+
+  if (!customElements.get('stream-from')) {
+    customElements.define('stream-from', StreamFromElement)
+  }
+
+  if (!customElements.get('updates-for')) {
+    customElements.define('updates-for', UpdatesForElement)
+  }
+}
+
+export {
+  Utils,
+  MorphCallbacks,
+  StreamFromElement,
+  UpdatesForElement,
+  SubscribingElement
+}
+
+export default {
+  perform,
+  performAsync,
+  shouldMorphCallbacks,
+  didMorphCallbacks,
+  initialize,
+  addOperation,
+  addOperations,
+  version,
+  get DOMOperations () {
+    console.warn(
+      'DEPRECATED: Please use `CableReady.operations` instead of `CableReady.DOMOperations`'
+    )
+    return OperationStore.all
+  },
+  get operations () {
+    return OperationStore.all
+  },
+  get consumer () {
+    return CableConsumer.getConsumer()
+  }
+}
